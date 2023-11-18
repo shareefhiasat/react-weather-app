49b13ebd11b92e50982d63d322cea4c4995ab779
shareef <shareef@sajilni.com>
2023-11-18 19:45:45 +0300

    add weather data logic

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const apiKey = "c57bb2610f0c11fb90388f14ba3a0c38";
+  const [weatherData, setWeatherData] = useState({});
+  const [city, setCity] = useState("");
+
+  const getWeather = (event) => {
+    if (event.key === "Enter") {
+      fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`
+      )
+        .then((response) => response.json())
+        .then((data) => {
+          setWeatherData(data);
+          setCity("");
+        });
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <input
+        className="input"
+        placeholder="Enter city name"
+        onChange={(e) => setCity(e.target.value)}
+        value={city}
+        onKeyDown={getWeather}
+      />
+
+      {typeof weatherData.main === "undefined" ? (
+        <div>
+          <p>
+            Welcome to weather app! Enter in a city to get the weather of, then
+            press enter.
+          </p>
+        </div>
+      ) : (
+        <div className="weather-data">
+          <p className="city">Weather in {weatherData.name}</p>
+          <p className="temp">{Math.round(weatherData.main.temp)}°C</p>
+          <p className="weather">{weatherData.weather[0].main}</p>
+        </div>
+      )}
+
+      {weatherData.cod == 404 && <p>City not found</p>}
     </div>
   );
-}
+};
 
 export default App;
